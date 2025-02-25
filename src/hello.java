01582e791b843764657756cbe29e78a0ec836dec
Shushma-Onefinitylabs <shushma@onefinitylabs.com>
2025-02-25 07:35:26 +0000

    tests removed

--- src/hello.java
+++ src/hello.java
@@ -6,7 +6,7 @@ public class Main {
             printMessage();
         } catch (Exception e) {
             System.err.println("An error occurred: " + e.getMessage());
-        
+        }
     }
     public static String printMessage() {
         String message = "Hello, World!!!";
@@ -18,18 +18,5 @@ public class Main {
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
-class MainTest {
 
-    @Test
-    void testPrintMessage() {
-        // Verify that the function returns the correct output
-        assertEquals("Hello, World!!!", Main.printMessage());
-    }
-
-    @Test
-    void testNoExceptionThrown() {
-        // Ensure that calling printMessage() does not throw an exception
-        assertDoesNotThrow(() -> Main.printMessage());
-    }
-}
 
