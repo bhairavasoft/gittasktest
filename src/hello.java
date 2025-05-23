7f259e302ce9b312b66298289880cc50d7be3e7a
bhairavasoft <167069374+bhairavasoft@users.noreply.github.com>
2025-05-23 11:37:39 +0000

    First

--- src/hello.java
+++ src/hello.java
@@ -4,7 +4,6 @@ public class Main {
     public static void main(String[] args) {
         try {
             printMessage();
-                        printMessage();
         } catch (Exception e) {
             System.err.println("An error occurred: " + e.getMessage());
         }
